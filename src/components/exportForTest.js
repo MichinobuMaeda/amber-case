8d7c9af7d4ab94c7f561fe57e9b3171e51bd7b37
Michinobu Maeda <michinobumaeda@gmail.com>
2021-11-21 22:33:03 +0900

    refactoring

--- src/components/exportForTest.js
+++ src/components/exportForTest.js
@@ -1,31 +1,30 @@
+// work around for mocking problem.
 import AccountsPanel from './AccountsPanel';
+import ControlledAccordion from './ControlledAccordion';
 import MyDisplayNamePanel from './MyDisplayNamePanel';
 import MyEmailPanel from './MyEmailPanel';
 import MyPasswordPanel from './MyPasswordPanel';
+import PageTitle from './PageTitle';
+import RadioButtons from './RadioButtons';
 import ReauthenticationPanel from './ReauthenticationPanel';
 import SignInWithEmailLinkPanel from './SignInWithEmailLinkPanel';
 import SignInWithPasswordPanel from './SignInWithPasswordPanel';
 import SignOutPanel from './SignOutPanel';
-import ThemeModePanel from './ThemeModePanel';
-
-import ControlledAccordion from './ControlledAccordion';
-import PageTitle from './PageTitle';
-import RadioButtons from './RadioButtons';
 import ShowPasswordAdornment from './ShowPasswordAdornment';
+import ThemeModePanel from './ThemeModePanel';
 
 export {
   AccountsPanel,
+  ControlledAccordion,
   MyDisplayNamePanel,
   MyEmailPanel,
   MyPasswordPanel,
+  PageTitle,
+  RadioButtons,
   ReauthenticationPanel,
   SignInWithEmailLinkPanel,
   SignInWithPasswordPanel,
   SignOutPanel,
-  ThemeModePanel,
-
-  ControlledAccordion,
-  PageTitle,
-  RadioButtons,
   ShowPasswordAdornment,
+  ThemeModePanel,
 };
